7dd2ddc46e7278cc0f4bc32769d3a2754963d223
zenos <zenoslin@outlook.com>
2019-01-13 02:04:01 +0800

    update

--- main.js
+++ main.js
@@ -7,61 +7,74 @@ const {
     dialog
 } = require('electron')
 
+// 保持对window对象的全局引用，如果不这么做的话，当JavaScript对象被
+// 垃圾回收的时候，window对象将会自动的关闭
 let mainWindow
 
 function createWindow() {
-    // Create the browser window.
+    // 创建浏览器窗口。
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600
     })
 
-    // and load the index.html of the app.
+    // 然后加载应用的 index.html。
     mainWindow.loadFile('index.html')
 
-    // Open the DevTools.
+    // 打开开发者工具
     mainWindow.webContents.openDevTools()
 
-    // Emitted when the window is closed.
+    // 当 window 被关闭，这个事件会被触发。
     mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
+        // 取消引用 window 对象，如果你的应用支持多窗口的话，
+        // 通常会把多个 window 对象存放在一个数组里面，
+        // 与此同时，你应该删除相应的元素。
         mainWindow = null
     })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// Electron 会在初始化后并准备
+// 创建浏览器窗口时，调用这个函数。
+// 部分 API 在 ready 事件触发后才能使用。
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
+// 当全部窗口关闭时退出。
 app.on('window-all-closed', function () {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
+    // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+    // 否则绝大部分应用及其菜单栏会保持激活。
     if (process.platform !== 'darwin') {
         app.quit()
     }
 })
 
 app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
+    // 在macOS上，当单击dock图标并且没有其他窗口打开时，
+    // 通常在应用程序中重新创建一个窗口。
     if (mainWindow === null) {
         createWindow()
     }
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-ipcMain.on('open-file-dialog', (event) => {
+// 在这个文件中，你可以续写应用剩下主进程代码。
+// 也可以拆分成几个文件，然后用 require 导入。
+ipcMain.on('open-file-dialog-input', (event) => {
     dialog.showOpenDialog({
         properties: ['openFile', 'openDirectory']
     }, (files) => {
         if (files) {
             console.log('发出完成信息')
-            event.sender.send('selected-directory', files)
+            event.sender.send('input-path', files)
+        }
+    })
+})
+
+ipcMain.on('open-file-dialog-output', (event) => {
+    dialog.showOpenDialog({
+        properties: ['openFile', 'openDirectory']
+    }, (files) => {
+        if (files) {
+            console.log('发出完成信息')
+            event.sender.send('output-path', files)
         }
     })
 })
--- renderer.js
+++ renderer.js
@@ -2,25 +2,43 @@ const {ipcRenderer} = require('electron')
 const imagemin = require('./src/imagemin').compass;
 // const tiny = require('./src/tinyPNG')
 
-const selectDirBtn = document.getElementById('select-directory')
+const inputBtn = document.getElementById('input-btn')
+const outputBtn = document.getElementById('output-btn')
+const inputPath = document.getElementById('input-path')
+const outputPath = document.getElementById('output-path')
 const tinyBtn = document.getElementById('tiny-btn')
-const inputPath = document.getElementById('selected-file')
+const logSpan = document.getElementById('log-span')
+
+const btnAAA = document.getElementById('aaa')
 
 var _inputPath = './ui'
+var _outputPath = "./ui/temp"
 
-selectDirBtn.addEventListener('click', (event) => {
-    console.log('点击按钮')
-    ipcRenderer.send('open-file-dialog')
+inputBtn.addEventListener('click', (event) => {
+    console.log('点击输入按钮')
+    ipcRenderer.send('open-file-dialog-input')
 })
 
-ipcRenderer.on('selected-directory', (event, path) => {
+outputBtn.addEventListener('click', (event) => {
+    console.log('点击输出按钮')
+    ipcRenderer.send('open-file-dialog-output')
+})
+
+ipcRenderer.on('input-path', (event, path) => {
     console.log(`收到完成信息 ${path}`)
     _inputPath = path
     inputPath.value = `${path}`
 })
 
+ipcRenderer.on('output-path', (event, path) => {
+    console.log(`收到完成信息 ${path}`)
+    _outputPath = path
+    outputPath.value = `${path}`
+})
+
 tinyBtn.addEventListener('click', (event) => {
-    _inputPath = inputPath.value;
-    imagemin(_inputPath, _inputPath + "/temp"); 
-    console.log('压缩完成')
+    _inputPath = inputPath.value
+    _outputPath = outputPath.value
+    imagemin(_inputPath, _outputPath)
+    logSpan.innerHTML = "压缩完成"
 })
