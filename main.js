f840cf6faf5f68a969cbc483c9d03fd4cdc56064
zenoslin <zenoslin@outlook.com>
2019-01-15 19:35:39 +0800

    update

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ function createWindow() {
     // 创建浏览器窗口。
     mainWindow = new BrowserWindow({
         width: 800,
-        height: 480,
-        resizable: false
+        height: 480
+        // resizable: false
     })
 
     // 然后加载应用的 index.html。
